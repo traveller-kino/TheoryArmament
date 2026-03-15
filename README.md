52da61446cabadb3620d96873850d22e8603dcae
traveller-kino <84192243+traveller-kino@users.noreply.github.com>
2026-03-14 22:23:16 -0400

    rotations of interest

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-# YAMETE KUDASTOP!
-This program currently doesn't work, so I am seperating the DPS Analysis and Rotation Finder (GA).
+# Rotation simulation (currently for F/P Mages)
+Note: this is not for "MapleStory at home" ;) (not that you'd roll F/P anyway), this is for the upcoming Classic World, with the information we know.
 
-Best of luck, F/P mages xx
+
+Current simulator results of interest:
+| Pattern / Mobs Targeted : DPS 	| 1         	| 2          	| 3          	| 4       	| 5         	|   	| AVG         	| MAX       	|
+|-------------------------------	|-----------	|------------	|------------	|---------	|-----------	|---	|-------------	|-----------	|
+| **ABABAB**                    	| 27.441666 	| 48.1979166 	| 68.9635416 	| 75.8875 	| 78.575    	|   	| 59.81312484 	| 78.575    	|
+| ABBB                          	| 25.0875   	| 47.364583  	| 69.3802083 	| 74.2    	| 76.028125 	|   	| 58.41208326 	| 76.028125 	|
+| BAA                           	| 26.690625 	| 45.1       	| 62.76666   	| 72      	| 73.603125 	|   	| 56.032082   	| 73.603125 	|
+| BAAA                          	| 27.0135   	| 44.1604    	| 60.73958   	| 71.75   	| 73.634375 	|   	| 55.459571   	| 73.634375 	|
+| ABx4                          	| 23.4      	| 44.5333    	| 65.4666    	| 68.7    	| 70.9      	|   	| 54.59998    	| 70.9      	|
+| ABB                           	| 23.554166 	| 42.79375   	| 62.675     	| 67.7625 	| 69.075    	|   	| 53.1720832  	| 69.075    	|
+| **Fire Arrow (B)**            	| 21.433333 	| 42.866666  	| 64.3       	| 63.7    	| 64.4      	|   	| 51.3399998  	| 64.4      	|
+| **Poison Brace (A)**          	| 26.625    	| 39.627083  	| 53.446875  	| 66.8625 	| 67.9875   	|   	| 50.9097916  	| 67.9875   	|
+| **Eco Mode**                  	| 15.58333  	| 27.6333    	| 40.64125   	| 45.75   	| 47.690625 	|   	| 35.459701   	| 47.690625 	|
+
+Special note: "eco mode" is casting Poison Brace & Fire Arrow, and waiting for the poison DoT to expire. While it isn't particularly good, it's interesting knowing it's only c. 40% less than other rotations.
+Alternating Poison Brace & Fire Arrow results in the highest DPS. This makes sense, especially in a "live" environment, as this maximizes instantaneous damage, and mobs with DoTs ticking on them (since more will be spawning in constantly).
--- damageDynamometer.py
+++ damageDynamometer.py
@@ -8,23 +8,9 @@ import pandas as pd
 import pickle
 import bz2
 import time
-
-spellDatabaseFile = open('./spellDb.json', 'r')
-spellDatabase = json.load(spellDatabaseFile)
-spellIndex = {}
-for spell in spellDatabase:
-    spellIndex[spell['SpellName']] = spell
-
-pass
-
-# Note need to stop editing spells directly, and use an associative array for tracking time expirations
-
-# Simulation Parameters
-SIM_TARGETS =               5           # Even in SIM, FoE! https://youtu.be/dB_PVPyn6n8
-SIM_ADVANCE_RETARD =        1.00        # Globally advance or retard spell timings (e.g. +10% attack speed => 1.10)
-SIM_TIMING_EPSILON =        10.00       # Timing step for simulation in milliseconds (i.e. dX in Calculus)
-SIM_DROP_CAST_CHANCE =      0.01        # Chance for spell to whiff
-SIM_RUNTIME =               600000.00    # How long to simulate the rotation
+from collections import deque
+import matplotlib
+import matplotlib.pyplot as plt
 
 """
 SpellDB Fields
@@ -36,6 +22,22 @@ TargetIdsAffected   Some effects hit "certain targets", while "avoiding others";
 SecondaryEffect     Secondary spell effects, as spells
 Unique              Whether or not the spell needs to wear off before re-applying
 """
+N_DEBUG = False
+eventLogAnchor = "[{Timestamp}]->[{Timestamp2}]\t{SpellName}\t{Activity}"
+
+spellDatabaseFile = open('./spellDb.json', 'r')
+spellDatabase = json.load(spellDatabaseFile)
+spellIndex = {}
+for spell in spellDatabase:
+    spellIndex[spell['SpellName']] = spell
+
+
+# Simulation Parameters
+SIM_TARGETS =               5           # Even in SIM, FoE! https://youtu.be/dB_PVPyn6n8
+SIM_ADVANCE_RETARD =        1.00        # Globally advance or retard spell timings (e.g. +10% attack speed => 1.10)
+SIM_TIMING_EPSILON =        1.000       # Timing step for simulation in milliseconds (i.e. dX in Calculus)
+SIM_DROP_CAST_CHANCE =      0.01        # Chance for spell to whiff
+SIM_RUNTIME =               300000.00    # How long to simulate the rotation
 
 def readRotation(filename):
     rotation = []
@@ -46,6 +48,99 @@ def readRotation(filename):
     theFile.close()
     return rotation
 
-readRotation('./rotation.csv')
+# NOTE: need to stop editing spells directly, and use an associative array for tracking time expirations
+# "new" timing code
+    # add active spell to queue | activeSpells = []
+    # New spells are always APPENDED to activeSpells
+    # add timing queue | spellTimers = []
+    # ..., so order is preserved
+    # this way, activeSpells and spellTimers indices always 'stay with each other'
+def simulateRotation(rotation):
+    currentTime =   0.00
+    terminalTime =  SIM_RUNTIME
 
-pass
+    damageAttributions = pd.DataFrame(columns=['Timestamp', 'SpellName', 'Damage'])
+
+    interdictedTill = -0.01
+    activeSpells = []
+    spellTimers = []
+
+    while currentTime < terminalTime:
+        spellsToRemove = []
+        timersToRemove = []
+        #for spell, timer in zip(activeSpells, spellTimers): # change to zip
+        for i in range(0, len(activeSpells)):
+            spellTimers[i] -= SIM_TIMING_EPSILON
+            if  spellTimers[i] <= 0.00:
+                    damageAttributions.loc[len(damageAttributions)] = {'Timestamp': currentTime, 'SpellName': activeSpells[i]['SpellName'], 'Damage': activeSpells[i]['Potency']*min(SIM_TARGETS,len(activeSpells[i]['TargetIdsAffected']))}
+                    spellsToRemove.append(activeSpells[i])
+                    timersToRemove.append(spellTimers[i])
+        for i in range(0,len(spellsToRemove)):
+            activeSpells.remove(spellsToRemove[i])
+            spellTimers.remove(timersToRemove[i])
+            print(eventLogAnchor.format(Timestamp=currentTime,Activity='Lapsed',SpellName=spellsToRemove[i]['SpellName'],Timestamp2='NULL')) if N_DEBUG else ''
+        spellsToRemove = []
+        timersToRemove = []
+
+        if currentTime <= interdictedTill: # Simulating animation lock
+            currentTime += SIM_TIMING_EPSILON
+            continue
+        else:
+            if random.uniform(0.00, 1.00) < SIM_DROP_CAST_CHANCE: 
+                interdictedTill = max(interdictedTill, currentTime + 1500)
+                print(eventLogAnchor.format(Timestamp=currentTime,Activity='DROPPED',SpellName='NULL',Timestamp2=interdictedTill)) if N_DEBUG else ''
+                continue
+
+            spell = rotation.pop(0)
+            if spell['Interdicting'] == True:
+                interdictedTill = currentTime + spell['Timing'][1]
+            activeSpells.append(spell)
+            spellTimers.append(spell['Timing'][1])
+            print(eventLogAnchor.format(Timestamp=currentTime,Activity='Activated',SpellName=spell['SpellName'],Timestamp2=currentTime+spell['Timing'][1])) if N_DEBUG else ''
+
+            frozenActiveSpells = activeSpells.copy() # Need copy or else latent effects will fail to apply beyond first tick
+            if spell.get('SecondaryEffect') == None: continue
+            for sSpell in spell.get('SecondaryEffect'):
+                sSpell['Timing'] = [ 0.00, spell['Timing'][1] + sSpell['TimingOffset'][1] ]
+                
+                suppress = False
+                if sSpell['Unique'] == True:
+                    for oSpell in frozenActiveSpells:
+                        if not oSpell.get('GroupId'): continue
+                        else: 
+                            if oSpell.get('GroupId') == sSpell.get('GroupId'): suppress = True
+                        #if oSpell['SpellName'] == sSpell['SpellName']: suppress = True
+                if not suppress: 
+                    activeSpells.append(sSpell)
+                    spellTimers.append(sSpell['Timing'][1])
+                    print(eventLogAnchor.format(Timestamp=currentTime,Activity='Applied',SpellName=sSpell['SpellName'],Timestamp2=currentTime+sSpell['Timing'][1])) if N_DEBUG else ''
+                else:
+                    print(eventLogAnchor.format(Timestamp=currentTime,Activity='SUPPRESSED',SpellName=sSpell['SpellName'],Timestamp2='NULL')) if N_DEBUG else ''
+                
+        currentTime += SIM_TIMING_EPSILON
+    return damageAttributions
+
+a = readRotation('./rotation.csv')
+b = simulateRotation(a)
+
+totalDamageWithAttribution = b.groupby('SpellName')['Damage'].sum().reset_index().values.tolist()
+totalGeneralDamage = b.sum(numeric_only=True).drop('Timestamp')
+totalGeneralDps = (SIM_ADVANCE_RETARD * totalGeneralDamage) / (SIM_RUNTIME / 1000)
+
+b['Timestamp'] = pd.to_datetime(b['Timestamp'],unit='ms')
+b['Cumulative Damage'] = b.groupby("SpellName")['Damage'].cumsum()
+for spell_name, entry_data in b.groupby("SpellName"):
+    plt.plot(entry_data['Timestamp'], entry_data['Cumulative Damage'], label=spell_name)
+plt.plot(b['Timestamp'], b['Damage'].cumsum(), label='Total')
+#b.plot(x='Timestamp', y='Cumulative Damage', kind='line')
+plt.legend(title="Damage Source")
+plt.title("Dynamometer Output for {n} Targets\nTotal = {total}\nDPS = {dps}".format(n=SIM_TARGETS,total=totalGeneralDamage.values[0],dps=totalGeneralDps.values[0]))
+plt.xlabel("Simulator Time", fontsize=12)
+plt.ylabel("Cumulative Damage", fontsize=12)
+plt.grid(True, alpha=0.5)
+plt.show()
+
+print(totalDamageWithAttribution)
+print(totalGeneralDamage)
+print(totalGeneralDps)
+
